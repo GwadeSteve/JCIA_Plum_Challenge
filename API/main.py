d72a9146107bada0c579900eeaa12da26267dbdc
GSteve12 <gwadesteve12@gmail.com>
2025-04-13 15:02:30 +0100

    Started API, Completed the predict endpoint for predicting on uploaded images

--- API/main.py
+++ API/main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from typing import List
+from Predictor.predictor_utilities.predict import predictor
 
 app = FastAPI()
 
@@ -15,14 +15,35 @@ app.add_middleware(
 )
 
 @app.post("/api/predict")
-async def predict(file: UploadFile = File(...)):
+async def predict_endpoint(file: UploadFile = File(...)):
     if not file:
-        return {
-            "error": "No file uploaded"
-            }
+        raise HTTPException(status_code=400, detail="No file uploaded")
 
-    content = await file.read()
-    return {"filename": file.filename}
+    if not file.content_type.startswith("image/"):
+        raise HTTPException(status_code=400, detail="Invalid file type. Only images are allowed.")
+
+    try:
+        contents = await file.read()
+        prediction_result = predictor.predict_image(contents, file.filename)
+
+        if prediction_result and "prediction" in prediction_result and prediction_result["prediction"]:
+            top_prediction = prediction_result["prediction"][0]
+            top_class_name = top_prediction["class"]
+            top_probability = top_prediction["probability"]
+            superclass_mapping = prediction_result["superclass_mapping"].get(top_class_name)
+
+            prediction_result["resume"] = {
+                "predicted_class": top_class_name,
+                "probability": top_probability,
+                "superclass": superclass_mapping
+            }
+        else:
+            prediction_result["resume"] = {"error": "Could not get top prediction."}
+
+        return prediction_result
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error processing image: {e}")
 
 @app.get("/")
 async def root():
