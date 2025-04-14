ea5b6382630c453290625e215a47a2d5483d6ab6
GSteve12 <gwadesteve12@gmail.com>
2025-04-14 22:29:25 +0100

    Navbar and directory structure up to date

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,25 @@
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import PredictionPage from './pages/PredictionPage/PredictionPage';
+import PresentationPage from './pages/PresentationPage/PresentationPage';
+import RealtimePage from './pages/RealtimePage/RealtimePage';
+import './App.css'
 
 function App() {
     return (
-      <div className="App">
-          <h1>PlumVision</h1>
-      </div>
+        <div className='App'> 
+            <Navbar />
+            <div className="Content">
+                <Routes>
+                    <Route path="/" element={<PresentationPage />} />
+                    <Route path="/prediction" element={<PredictionPage />} />
+                    <Route path="/realtime" element={<RealtimePage />} />
+                </Routes>
+            </div>
+            <Footer />
+        </div>
     );
 }
 
---
+++ frontend/src/pages/PredictionPage/PredictionPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './PredictionPage.css'
+
+const PredictionPage = () => {
+  return (
+    <div>PredictionPage</div>
+  )
+}
+
+export default PredictionPage
