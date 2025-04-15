9aeb6d5bdbf7553d71013297ac125dab18b16805
GSteve12 <gwadesteve12@gmail.com>
2025-04-15 17:03:00 +0100

    Setup test to verify if app renders correctly

--- frontend/src/App.test.jsx
+++ frontend/src/App.test.jsx
@@ -1,19 +1,19 @@
 import { BrowserRouter as Router } from 'react-router-dom';
-  import { render } from '@testing-library/react';
-  import App from './App';
+import { render } from '@testing-library/react';
+import App from './App';
 
-  jest.mock('react-fast-marquee', () => {
-    return {
-      __esModule: true,
-      default: ({ children }) => <div>{children}</div>,
-    };
-  });
+jest.mock('aos', () => ({
+  init: jest.fn(),
+  refresh: jest.fn()
+}));
+
+
+test('renders app without crashing', () => {
+  const { container } = render(
+    <Router>
+      <App />
+    </Router>
+  );
   
-  test('renders learn react link', () => {
-      render(
-          <Router>
-              <App />
-          </Router>
-      );
-  });
-  
+  expect(container).toBeDefined();
+});
